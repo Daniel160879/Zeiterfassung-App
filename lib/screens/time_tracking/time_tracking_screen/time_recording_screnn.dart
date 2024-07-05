f6f84f5a536c3ff2ad0a7bc06d29a61d0ae41e30
Daniel <danielhandel4@gmail.com>
2024-07-05 12:12:17 +0200

    Timelist ready

--- lib/screens/time_tracking/time_tracking_screen/time_recording_screnn.dart
+++ lib/screens/time_tracking/time_tracking_screen/time_recording_screnn.dart
@@ -4,7 +4,7 @@ import 'package:meine_zeiterfassungs_app/screens/auth/repository/auth_repo.dart'
 import 'package:meine_zeiterfassungs_app/screens/time_tracking/projectScreen/data/project.dart';
 import 'package:meine_zeiterfassungs_app/screens/time_tracking/time_tracking_screen/itemModel/stop_watch.dart';
 import 'package:meine_zeiterfassungs_app/screens/time_tracking/time_tracking_screen/itemModel/pro_u_arbeitsplatz.dart';
-import 'package:meine_zeiterfassungs_app/screens/time_tracking/time_tracking_screen/itemModel/workingday_list.dart';
+import 'package:meine_zeiterfassungs_app/screens/time_tracking/time_tracking_screen/itemModel/time_list.dart';
 import 'package:meine_zeiterfassungs_app/screens/time_tracking/time_tracking_screen/repository/time_firebase_repository.dart';
 import 'package:meine_zeiterfassungs_app/screens/time_tracking/working_space_screen/data/workplace.dart';
 import 'package:provider/provider.dart';
@@ -22,9 +22,9 @@ class TimeRecordingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
+    return ChangeNotifierProvider<TimerecordingProvider>(
       create: (context) => TimerecordingProvider(FirebaseTimeRepository(), AuthRepository(), project, workPlace),
-      child: Scaffold(
+      builder: (context, child) => Scaffold(
         appBar: AppBar(
           title: const Text('Zeiterfassung', style: myAppBarTextStyle),
           centerTitle: true,
@@ -33,27 +33,36 @@ class TimeRecordingScreen extends StatelessWidget {
         body: Container(
           decoration: myBoxdeco,
           child: Center(
-            child: ListView(children: [
-              Column(
-                children: [
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  TimeRecordingItem(project: project, workPlace: workPlace),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  StopWatch(
-                    project: project,
-                    workPlace: workPlace,
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  const TimeList(),
-                ],
-              ),
-            ]),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                TimeRecordingItem(project: project, workPlace: workPlace),
+                const SizedBox(
+                  height: 50,
+                ),
+                StopWatch(
+                  project: project,
+                  workPlace: workPlace,
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                ElevatedButton(
+                    style: const ButtonStyle(backgroundColor: WidgetStatePropertyAll(Color.fromARGB(255, 80, 73, 72))),
+                    onPressed: () {
+                      final myModel = context.read<TimerecordingProvider>();
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  ChangeNotifierProvider.value(value: myModel, child: const TimeList())));
+                    },
+                    child: const Text(
+                      'Arbeitszeiten',
+                      style: myTextStyle,
+                    ))
+              ],
+            ),
           ),
         ),
       ),
